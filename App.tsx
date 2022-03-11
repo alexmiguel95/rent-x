c5c9f06c99e57e31db178cf744909e4b31893d3f
Alex Miguel <alexmiguel95@gmail.com>
2022-03-11 18:03:16 -0300

    theme default was created

--- App.tsx
+++ App.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import Home from './src/screens/Home';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/theme/theme';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
-    return <Home />;
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular, 
+        Inter_500Medium,
+        Archivo_400Regular, 
+        Archivo_500Medium, 
+        Archivo_600SemiBold
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Home />
+        </ThemeProvider>
+    );
 }
