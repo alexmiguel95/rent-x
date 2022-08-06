ba063656222733174214ed334858c883db60e7ec
Alex Miguel <alexm@esparta.io>
2022-08-05 21:29:57 -0300

    scheduling details scrren was created

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import theme from './src/theme/theme';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo';
 import AppLoading from 'expo-app-loading';
-import Scheduling from './src/screens/Scheduling';
+import SchedulingDetails from './src/screens/SchedulingDetails';
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -21,7 +21,7 @@ export default function App() {
 
     return (
         <ThemeProvider theme={theme}>
-            <Scheduling />
+            <SchedulingDetails />
         </ThemeProvider>
     );
 }
